2ad02c6be3a5201eac03aa9f32ac35dd6e8b1fc6
Rafael Alejandro Dorado Moreno <55125249+alxdom@users.noreply.github.com>
2020-04-15 23:10:10 -0500

    Se cambio el numero

--- js/api.js
+++ js/api.js
@@ -7,7 +7,7 @@ function envMsg(){
     var street = document.getElementById("calle").value;
     var number = document.getElementById("numero").value;
     
-    let url = "https://api.whatsapp.com/send?phone=523861055227&text=*PURIFICADORA DE AGUA SAN IGNACIO*%0A*Servicio de pedidos*%0A%0A%0A*Nombre:* %0A" 
+    let url = "https://api.whatsapp.com/send?phone=523861111779&text=*PURIFICADORA DE AGUA SAN IGNACIO*%0A*Servicio de pedidos*%0A%0A%0A*Nombre:* %0A" 
                 + name + 
                 "%0A*Numero de garrafones:* %0A"
                 + water +
